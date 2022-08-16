d5bb17091718cf7c133450b0bc15b9d8fbaa0e85
kruegert <krueger@g.harvard.edu>
2022-08-16 15:27:18 -0400

    added option to specify port via command line.

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     # use port 8000 if no port is specified via command line argument
     port = 8000 if len(sys.argv) < 2 or not str.isdigit(sys.argv[1]) else sys.argv[1]
     print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port))
-    serve(app, host='0.0.0.0', port=8000, max_request_body_size=107374182400, max_request_header_size=8589934592)
+    serve(app, host='0.0.0.0', port=port, max_request_body_size=107374182400, max_request_header_size=8589934592)
